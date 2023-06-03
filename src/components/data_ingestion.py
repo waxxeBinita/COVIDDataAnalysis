cc3f9f680fcc337a8fb4f79622c2c7f9440b67e0
waxxeBinita <binita.bbshrestha@gmail.com>
2023-06-03 15:42:49 +0100

    data analysis contines

---
+++ src/components/data_ingestion.py
@@ -0,0 +1,11 @@
+import os
+import sys
+import pandas as pd
+
+from src.exception import CustomException
+from src.logger import logging
+from sklearn.model_selection import train_test_split
+from dataclasses import dataclasses
+
+
+
